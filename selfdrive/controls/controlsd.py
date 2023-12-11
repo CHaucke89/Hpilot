648283ba0b8488146d715c7037654f074e68166e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-12-11 11:57:53 -0700

    Only show lines when lateral is active

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -764,8 +764,8 @@ class Controls:
     hudControl.lanesVisible = self.enabled
     hudControl.leadVisible = self.sm['longitudinalPlan'].hasLead
 
-    hudControl.rightLaneVisible = True
-    hudControl.leftLaneVisible = True
+    hudControl.rightLaneVisible = CC.latActive
+    hudControl.leftLaneVisible = CC.latActive
 
     recent_blinker = (self.sm.frame - self.last_blinker_frame) * DT_CTRL < 5.0  # 5s blinker cooldown
     ldw_allowed = self.is_ldw_enabled and CS.vEgo > LDW_MIN_SPEED and not recent_blinker \
