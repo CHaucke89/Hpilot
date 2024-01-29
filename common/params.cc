12825e58ab6857eee092dc968bba3987388078f3
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-28 22:05:39 -0700

    Retain tethering status between reboots

--- common/params.cc
+++ common/params.cc
@@ -324,6 +324,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"StandardFollow", PERSISTENT},
     {"StandardJerk", PERSISTENT},
     {"StoppingDistance", PERSISTENT},
+    {"TetheringEnabled", PERSISTENT},
     {"UnlimitedLength", PERSISTENT},
     {"UpdateSchedule", PERSISTENT},
     {"UpdateTime", PERSISTENT},
--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -15,6 +15,7 @@
 #include "common/mat.h"
 #include "common/params.h"
 #include "common/timing.h"
+#include "selfdrive/ui/qt/network/wifi_manager.h"
 #include "system/hardware/hw.h"
 
 const int UI_BORDER_SIZE = 30;
@@ -201,6 +202,7 @@ typedef struct UIScene {
   bool reverse_cruise_ui;
   bool road_name_ui;
   bool show_driver_camera;
+  bool tethering_enabled;
   bool turn_signal_left;
   bool turn_signal_right;
   bool unlimited_road_ui_length;
@@ -267,6 +269,8 @@ private:
   QTimer *timer;
   bool started_prev = false;
   PrimeType prime_type = PrimeType::UNKNOWN;
+
+  WifiManager *wifi = nullptr;
 };
 
 UIState *uiState();
