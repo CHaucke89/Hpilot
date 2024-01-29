82a24da4382e35ebe97aecd1b22783bce79b8943
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-28 22:05:39 -0700

    Customize the model visuals in the onroad UI
    Added toggles to customize the lane lines, path, road edges, path edges, and an "Unlimited Length" mode that extends the road UI out as far as the model can see.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -180,12 +180,18 @@ typedef struct UIScene {
   bool custom_theme;
   bool enabled;
   bool experimental_mode;
+  bool model_ui;
   bool quality_of_life_controls;
   bool quality_of_life_visuals;
   bool turn_signal_left;
   bool turn_signal_right;
+  bool unlimited_road_ui_length;
+  float lane_line_width;
   float lane_width_left;
   float lane_width_right;
+  float path_edge_width;
+  float path_width;
+  float road_edge_width;
   int camera_view;
   int conditional_speed;
   int conditional_speed_lead;
@@ -194,6 +200,7 @@ typedef struct UIScene {
   int custom_icons;
   int custom_signals;
   QPolygonF track_adjacent_vertices[6];
+  QPolygonF track_edge_vertices;
 
 } UIScene;
 
