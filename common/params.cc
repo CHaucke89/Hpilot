466fc1dee5ca895b8463f7a9dbc8c54932ee22be
James <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    FrogsGoMoo's tune

--- common/params.cc
+++ common/params.cc
@@ -263,6 +263,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ForceFingerprint", PERSISTENT},
     {"FPSCounter", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
+    {"FrogsGoMooTune", PERSISTENT},
     {"GoatScream", PERSISTENT},
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -275,6 +275,25 @@ class CarInterface(CarInterfaceBase):
       tune.kpV = [1.]
       tune.kiBP = [0.]
       tune.kiV = [1.]
+    elif params.get_bool("FrogsGoMooTune"):
+      tune.deadzoneBP = [0., 16., 20., 30.]
+      tune.deadzoneV = [0., .03, .06, .15]
+      tune.kpBP = [0., 5., 20.]
+      tune.kpV = [1.3, 1.0, 0.7]
+
+      # In MPH  = [ 0.,   2,    5,   11,   27,  45,  52,  67,   90] ​​
+      tune.kiBP = [ 0.,  1.,   2.,   5.,  12., 20., 23., 30.,  40.]
+      tune.kiV =  [.33, .33, .313, .245, .215, .17, .10, .01, .001]
+
+      if candidate in TSS2_CAR:
+        ret.stopAccel = -0.40
+        ret.stoppingDecelRate = 0.009  # reach stopping target smoothly
+      else:
+        ret.stopAccel = -2.5  # on stock Toyota this is -2.5
+        ret.stoppingDecelRate = 0.3  # This is okay for TSS-P
+
+      ret.vEgoStarting = 0.1
+      ret.vEgoStopping = 0.1
     elif (candidate in TSS2_CAR or ret.enableGasInterceptor) and params.get_bool("DragonPilotTune"):
       # TSS2 tune - Credit goes to the DragonPilot team!
       tune.kpBP = [0., 5., 20.]
