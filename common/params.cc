63f8a87ea6161a3e398b808f8d1f4c00fe5bc30b
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    DragonPilot's Toyota tune
    Co-Authored-By: eFini <16603033+efinilan@users.noreply.github.com>
    Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -246,6 +246,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DeviceShutdown", PERSISTENT},
     {"DisableOnroadUploads", PERSISTENT},
     {"DisengageVolume", PERSISTENT},
+    {"DragonPilotTune", PERSISTENT},
     {"DriveStats", PERSISTENT},
     {"EngageVolume", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
