0ab11cd7201686fdc553eb4f19eec92a64171e5a
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    Cydia's Toyota tune
    Co-Authored-By: Irene <12470297+cydia2020@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -242,6 +242,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CustomSignals", PERSISTENT},
     {"CustomSounds", PERSISTENT},
     {"CustomTheme", PERSISTENT},
+    {"CydiaTune", PERSISTENT},
     {"DisengageVolume", PERSISTENT},
     {"DriveStats", PERSISTENT},
     {"EngageVolume", PERSISTENT},
