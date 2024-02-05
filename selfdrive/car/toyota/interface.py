14928b1ea96fe71530f615cebf3f51628ef0e2df
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    DragonPilot's Toyota tune
    Co-Authored-By: eFini <16603033+efinilan@users.noreply.github.com>
    Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -275,6 +275,18 @@ class CarInterface(CarInterfaceBase):
       tune.kpV = [1.]
       tune.kiBP = [0.]
       tune.kiV = [1.]
+    elif (candidate in TSS2_CAR or ret.enableGasInterceptor) and params.get_bool("DragonPilotTune"):
+      # TSS2 tune - Credit goes to the DragonPilot team!
+      tune.kpBP = [0., 5., 20.]
+      tune.kpV = [1.3, 1.0, 0.7]
+      # In MPH  = [ 0.,   2,    5,   11,   27,  45,  52,  67,   90] ​​
+      tune.kiBP = [ 0.,  1.,   2.,   5.,  12., 20., 23., 30.,  40.]
+      tune.kiV =  [.33, .33, .313, .245, .215, .17, .10, .01, .001]
+      if candidate in TSS2_CAR:
+        ret.vEgoStopping = 0.1         # car is near 0.1 to 0.2 when car starts requesting stopping accel
+        ret.vEgoStarting = 0.1         # needs to be > or == vEgoStopping
+        ret.stopAccel = -0.40          # Toyota requests -0.4 when stopped
+        ret.stoppingDecelRate = 0.5    # reach stopping target smoothly
     elif candidate in TSS2_CAR or ret.enableGasInterceptor:
       tune.kpBP = [0., 5., 20.]
       tune.kpV = [1.3, 1.0, 0.7]
