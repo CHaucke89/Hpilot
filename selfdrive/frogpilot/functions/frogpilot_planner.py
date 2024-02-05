8e37ff41e88d708deba92d39b23402f9997490ea
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-05 12:33:48 -0700

    Blind spot path
    Added toggle to display a red path in the adjacent lane when a vehicle is detected in the blind spot.

--- selfdrive/frogpilot/functions/frogpilot_planner.py
+++ selfdrive/frogpilot/functions/frogpilot_planner.py
@@ -1,6 +1,7 @@
 import cereal.messaging as messaging
 
 from openpilot.common.conversions import Conversions as CV
+from openpilot.selfdrive.controls.lib.desire_helper import LANE_CHANGE_SPEED_MIN
 from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, get_max_accel
 
 from openpilot.selfdrive.frogpilot.functions.frogpilot_functions import FrogPilotFunctions
@@ -32,6 +33,15 @@ class FrogPilotPlanner:
     # Update the max allowed speed
     self.v_cruise = self.update_v_cruise(carState, controlsState, enabled, modelData, v_cruise, v_ego)
 
+    # Lane detection
+    check_lane_width = self.adjacent_lanes or self.blind_spot_path
+    if check_lane_width and v_ego >= LANE_CHANGE_SPEED_MIN:
+      self.lane_width_left = float(self.fpf.calculate_lane_width(modelData.laneLines[0], modelData.laneLines[1], modelData.roadEdges[0]))
+      self.lane_width_right = float(self.fpf.calculate_lane_width(modelData.laneLines[3], modelData.laneLines[2], modelData.roadEdges[1]))
+    else:
+      self.lane_width_left = 0
+      self.lane_width_right = 0
+
   def update_v_cruise(self, carState, controlsState, enabled, modelData, v_cruise, v_ego):
     return min(v_cruise)
 
@@ -40,12 +50,16 @@ class FrogPilotPlanner:
     frogpilot_plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState'])
     frogpilotPlan = frogpilot_plan_send.frogpilotPlan
 
+    frogpilotPlan.laneWidthLeft = self.lane_width_left
+    frogpilotPlan.laneWidthRight = self.lane_width_right
+
     pm.send('frogpilotPlan', frogpilot_plan_send)
 
   def update_frogpilot_params(self, params, params_memory):
     self.is_metric = params.get_bool("IsMetric")
 
     custom_ui = params.get_bool("CustomUI")
+    self.blind_spot_path = params.get_bool("BlindSpotPath") and custom_ui
 
     longitudinal_tune = params.get_bool("LongitudinalTune")
     self.acceleration_profile = params.get_int("AccelerationProfile") if longitudinal_tune else 0
