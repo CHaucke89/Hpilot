5ace1ebed241d6f2a6a3c415682b08b8e07e71fe
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Sidebar retains its previous display when going onroad
    Added function to retain the previous sidebar display when going onroad.

--- common/params.cc
+++ common/params.cc
@@ -363,6 +363,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ShowMemoryUsage", PERSISTENT},
     {"ShowStorageLeft", PERSISTENT},
     {"ShowStorageUsed", PERSISTENT},
+    {"Sidebar", PERSISTENT},
     {"StandardFollow", PERSISTENT},
     {"StandardJerk", PERSISTENT},
     {"StandbyMode", PERSISTENT},
