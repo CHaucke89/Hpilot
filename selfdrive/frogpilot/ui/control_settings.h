8528154eaca6998fcb50d76c103733371c2a74eb
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Aggressive acceleration when following a lead
    Added toggle to make the acceleration more aggressive when following a lead.

--- selfdrive/frogpilot/ui/control_settings.h
+++ selfdrive/frogpilot/ui/control_settings.h
@@ -33,7 +33,7 @@ private:
   std::set<QString> fireTheBabysitterKeys = {};
   std::set<QString> laneChangeKeys = {};
   std::set<QString> lateralTuneKeys = {};
-  std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "DecelerationProfile"};
+  std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "DecelerationProfile", "AggressiveAcceleration"};
   std::set<QString> mtscKeys = {};
   std::set<QString> qolKeys = {};
   std::set<QString> speedLimitControllerKeys = {};
