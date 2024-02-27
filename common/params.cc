edb2d6761b731c9b5263db4b3de26cc77b83ab0b
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    "Soft" reboot button
    Add function to perform a "soft" reboot for quicker reboots.
    
    Co-Authored-By: CHaucke89 <132518562+chaucke89@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -258,6 +258,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DisableOnroadUploads", PERSISTENT},
     {"DisableOpenpilotLongitudinal", PERSISTENT},
     {"DisengageVolume", PERSISTENT},
+    {"DoSoftReboot", CLEAR_ON_MANAGER_START},
     {"DragonPilotTune", PERSISTENT},
     {"DriverCamera", PERSISTENT},
     {"DriveStats", PERSISTENT},
--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -415,7 +415,7 @@ def manager_thread() -> None:
 
     # Exit main loop when uninstall/shutdown/reboot is needed
     shutdown = False
-    for param in ("DoUninstall", "DoShutdown", "DoReboot"):
+    for param in ("DoUninstall", "DoShutdown", "DoReboot", "DoSoftReboot"):
       if params.get_bool(param):
         shutdown = True
         params.put("LastManagerExitReason", f"{param} {datetime.datetime.now()}")
@@ -491,6 +491,9 @@ def main() -> None:
   if params.get_bool("DoUninstall"):
     cloudlog.warning("uninstalling")
     HARDWARE.uninstall()
+  elif params.get_bool("DoSoftReboot"):
+    cloudlog.warning("softreboot")
+    HARDWARE.soft_reboot()
   elif params.get_bool("DoReboot"):
     cloudlog.warning("reboot")
     HARDWARE.reboot()
