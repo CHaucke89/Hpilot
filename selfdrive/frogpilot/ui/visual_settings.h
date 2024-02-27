33622f407c5584b6a68c5c4b4273ce81cc0f1e78
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Stylistic map UIs and MapBox styles
    Fancied up the map in the onroad UI and added toggles to use a custom MapBox style when using "Navigate on openpilot",
    
    Co-Authored-By: mike8643 <98910897+mike8643@users.noreply.github.com>
    Co-Authored-By: pencilpusher <83676301+jakethesnake420@users.noreply.github.com>
    Co-Authored-By: multikyd <72329880+multikyd@users.noreply.github.com>

--- selfdrive/frogpilot/ui/visual_settings.h
+++ selfdrive/frogpilot/ui/visual_settings.h
@@ -28,12 +28,14 @@ private:
   void updateState(const UIState &s);
   void updateToggles();
 
+  ButtonControl *mapStyleButton;
+
   std::set<QString> alertVolumeControlKeys = {"EngageVolume", "DisengageVolume", "RefuseVolume", "PromptVolume", "PromptDistractedVolume", "WarningSoftVolume", "WarningImmediateVolume"};
   std::set<QString> customAlertsKeys = {"GreenLightAlert", "LeadDepartingAlert", "LoudBlindspotAlert", "SpeedLimitChangedAlert"};
   std::set<QString> customOnroadUIKeys = {"AccelerationPath", "AdjacentPath", "BlindSpotPath", "FPSCounter", "LeadInfo", "PedalsOnUI", "RoadNameUI"};
   std::set<QString> customThemeKeys = {"HolidayThemes", "CustomColors", "CustomIcons", "CustomSignals", "CustomSounds"};
   std::set<QString> modelUIKeys = {"DynamicPathWidth", "HideLeadMarker", "LaneLinesWidth", "PathEdgeWidth", "PathWidth", "RoadEdgesWidth", "UnlimitedLength"};
-  std::set<QString> qolKeys = {"DriveStats", "FullMap", "HideSpeed"};
+  std::set<QString> qolKeys = {"DriveStats", "FullMap", "HideSpeed", "MapStyle"};
   std::set<QString> screenKeys = {"HideUIElements", "ScreenBrightness", "ScreenBrightnessOnroad", "ScreenRecorder", "ScreenTimeout", "ScreenTimeoutOnroad", "StandbyMode"};
 
   std::map<std::string, ParamControl*> toggles;
