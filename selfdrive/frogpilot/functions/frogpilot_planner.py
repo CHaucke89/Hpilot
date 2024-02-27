66444a7ad5b5c82a62d7c0a354db88548b837f5d
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Increase stop distance
    Added toggle to increase the stop distance from lead vehicles.

--- selfdrive/frogpilot/functions/frogpilot_planner.py
+++ selfdrive/frogpilot/functions/frogpilot_planner.py
@@ -69,7 +69,7 @@ class FrogPilotPlanner:
     self.road_curvature = self.fpf.road_curvature(modelData, v_ego)
 
     # Update the desired stopping distance
-    self.stop_distance = STOP_DISTANCE
+    self.stop_distance = STOP_DISTANCE + self.increased_stopping_distance
 
     # Update the max allowed speed
     self.v_cruise = self.update_v_cruise(carState, controlsState, enabled, modelData, v_cruise, v_ego)
@@ -135,3 +135,4 @@ class FrogPilotPlanner:
     self.acceleration_profile = params.get_int("AccelerationProfile") if longitudinal_tune else 0
     self.deceleration_profile = params.get_int("DecelerationProfile") if longitudinal_tune else 0
     self.aggressive_acceleration = longitudinal_tune and params.get_bool("AggressiveAcceleration")
+    self.increased_stopping_distance = params.get_int("StoppingDistance") * (1 if self.is_metric else CV.FOOT_TO_METER) if longitudinal_tune else 0
