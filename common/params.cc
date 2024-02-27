8cd345b8fba4c2c1d89c3718271df571f89e915e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Show IP in sidebar

--- common/params.cc
+++ common/params.cc
@@ -359,6 +359,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"SilentMode", PERSISTENT},
     {"ShowCPU", PERSISTENT},
     {"ShowGPU", PERSISTENT},
+    {"ShowIP", PERSISTENT},
     {"ShowMemoryUsage", PERSISTENT},
     {"ShowStorageLeft", PERSISTENT},
     {"ShowStorageUsed", PERSISTENT},
