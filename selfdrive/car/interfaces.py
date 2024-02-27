6321b745ff5cb1cbe9e25054753b42ed7d3dd466
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Disable below steer speed event after it's been shown

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -102,6 +102,9 @@ class CarInterfaceBase(ABC):
     # FrogPilot variables
     params = Params()
 
+    self.belowSteerSpeed_shown = False
+    self.disable_belowSteerSpeed = False
+
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed, frogpilot_variables):
     if frogpilot_variables.sport_plus:
