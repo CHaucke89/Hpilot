c0ee5cbcc6c129cd36c86810dbddfba9828ebf98
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Device metrics in the sidebar
    Added functions to enable device monitoring in the sidebar by tapping on their respective metric boxes.

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -19,7 +19,7 @@ from openpilot.common.params import Params
 from openpilot.common.realtime import DT_TRML
 from openpilot.selfdrive.controls.lib.alertmanager import set_offroad_alert
 from openpilot.system.hardware import HARDWARE, TICI, AGNOS
-from openpilot.system.loggerd.config import get_available_percent
+from openpilot.system.loggerd.config import get_available_bytes, get_available_percent, get_used_bytes
 from openpilot.selfdrive.statsd import statlog
 from openpilot.common.swaglog import cloudlog
 from openpilot.selfdrive.thermald.power_monitoring import PowerMonitoring
@@ -243,6 +243,9 @@ def thermald_thread(end_event, hw_queue) -> None:
       pass
 
     fpmsg = messaging.new_message('frogpilotDeviceState')
+
+    fpmsg.frogpilotDeviceState.freeSpace = round(get_available_bytes(default=32.0 * (2 ** 30)) / (2 ** 30))
+    fpmsg.frogpilotDeviceState.usedSpace = round(get_used_bytes(default=0.0 * (2 ** 30)) / (2 ** 30))
 
     pm.send("frogpilotDeviceState", fpmsg)
 
