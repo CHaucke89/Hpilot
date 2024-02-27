7f8f0cf6c65c4fd36e117697ab20bb05df4e290c
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Force auto tune

--- selfdrive/frogpilot/ui/control_settings.h
+++ selfdrive/frogpilot/ui/control_settings.h
@@ -39,7 +39,7 @@ private:
   std::set<QString> experimentalModeActivationKeys = {"ExperimentalModeViaDistance", "ExperimentalModeViaLKAS", "ExperimentalModeViaScreen"};
   std::set<QString> fireTheBabysitterKeys = {"NoLogging", "MuteOverheated", "NoUploads"};
   std::set<QString> laneChangeKeys = {};
-  std::set<QString> lateralTuneKeys = {};
+  std::set<QString> lateralTuneKeys = {"ForceAutoTune"};
   std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "DecelerationProfile", "AggressiveAcceleration"};
   std::set<QString> mtscKeys = {};
   std::set<QString> qolKeys = {"DisableOnroadUploads"};
