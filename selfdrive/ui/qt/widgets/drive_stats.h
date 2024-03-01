1b38247c88dfde0baf437be6fae52622785a9f21
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-03-01 09:35:33 -0700

    Track FrogPilot drives

--- selfdrive/ui/qt/widgets/drive_stats.h
+++ selfdrive/ui/qt/widgets/drive_stats.h
@@ -3,6 +3,8 @@
 #include <QJsonDocument>
 #include <QLabel>
 
+#include "common/params.h"
+
 class DriveStats : public QFrame {
   Q_OBJECT
 
@@ -15,10 +17,11 @@ private:
   inline QString getDistanceUnit() const { return metric_ ? tr("KM") : tr("Miles"); }
 
   bool metric_;
+  Params params;
   QJsonDocument stats_;
   struct StatsLabels {
     QLabel *routes, *distance, *distance_unit, *hours;
-  } all_, week_;
+  } all_, week_, frogPilot_;
 
 private slots:
   void parseResponse(const QString &response, bool success);
