fa19a206f8e5b23576ea4dbb7a722de44625298c
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-03-06 18:53:46 -0700

    Screen Management
    Added toggles to adjust the screen brightness, timeout settings, and other UI changes.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -176,6 +176,7 @@ typedef struct UIScene {
 
   // FrogPilot variables
   bool acceleration_path;
+  bool active_alert;
   bool adjacent_path;
   bool adjacent_path_metrics;
   bool always_on_lateral;
@@ -194,6 +195,12 @@ typedef struct UIScene {
   bool fahrenheit;
   bool fps_counter;
   bool full_map;
+  bool hide_alerts;
+  bool hide_aol_status_bar;
+  bool hide_cem_status_bar;
+  bool hide_lead_marker;
+  bool hide_map_icon;
+  bool hide_max_speed;
   bool hide_speed;
   bool hide_speed_ui;
   bool holiday_themes;
@@ -207,8 +214,11 @@ typedef struct UIScene {
   bool reverse_cruise_ui;
   bool road_name_ui;
   bool rotating_wheel;
+  bool screen_recorder;
   bool show_driver_camera;
+  bool standby_mode;
   bool standstill;
+  bool status_changed;
   bool tethering_enabled;
   bool turn_signal_left;
   bool turn_signal_right;
@@ -237,6 +247,9 @@ typedef struct UIScene {
   int obstacle_distance;
   int obstacle_distance_stock;
   int screen_brightness;
+  int screen_brightness_onroad;
+  int screen_timeout;
+  int screen_timeout_onroad;
   int steering_angle_deg;
   int stopped_equivalence;
 
@@ -270,7 +283,10 @@ public:
 
   QTransform car_space_transform;
 
+  // FrogPilot variables
   WifiManager *wifi = nullptr;
+
+  UIStatus previous_status;
 
 signals:
   void uiUpdate(const UIState &s);
@@ -319,7 +335,7 @@ signals:
   void interactiveTimeout();
 
 public slots:
-  void resetInteractiveTimeout(int timeout = -1);
+  void resetInteractiveTimeout(int timeout = -1, int timeout_onroad = -1);
   void update(const UIState &s);
 };
 
